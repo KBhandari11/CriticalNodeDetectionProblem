9b7bc771436261ea8f0974296474b9b65ee75886
KBhandari11 <kushalbhandari2013.kb@gmail.com>
2025-11-07 19:16:46 -0500

    Drop files from .gitignore

--- benchmark.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-mv GDM_CN.tar ./GDM/GDM_CN
-#tar -xvf ./GDM/GDM_CN/GDM_CN.tar
-mv GDM_ND.tar ./GDM/
-#tar -xvf ./GDM/GDM_ND.tar
-mv CN.tar ./FINDER/FINDER_CN 
-#tar -xvf ./FINDER/FINDER_CN/CN.tar *.txt
-mv ND.tar ./FINDER/FINDER_ReTrained
-#tar -xvf ./FINDER/FINDER_ReTrained/ND.tar
